01d28df8cc1bbb66359d81fb77f4229e0e46ceef
Arif <arifkutq63@gmail.com>
2023-01-11 23:31:28 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 
 
 function App() {
-  const [number, setNumber] = useState("")
+  const [number, setNumber] = useState("");
+  const valid = useMemo(() => valid(number), [number]);
   return (
     <div className="App">
       <div className="control has-icons-right">
@@ -23,17 +24,17 @@ function App() {
         <span className="icon is-small is-right">
           <i className="fas fa-times" id="MyElement" />
         </span>
-        <button onClick={validateNumber}>Check</button>
+        <button onClick={valid}>Check</button>
       </div>
     </div>
 
   );
-  function validateNumber() {
-    if (!number.match(/^\d+/))
-    {
-      document.getElementById("MyElement").className = "fas fa-check";
-    }
-  }
+  // function validateNumber() {
+  //   if (!number.match(/^\d+/))
+  //   {
+  //     document.getElementById("MyElement").className = "fas fa-check";
+  //   }
+  // }
 
 }
 
