4c02925c80ef113e03e666624f983e0592e3e4a4
Arif <arifkutq63@gmail.com>
2023-01-11 13:23:15 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
+import { useState } from "react";
+import { validNumber } from "./Regex";
 
 function App() {
+  const [number, setNumber] =useState("")
+  const [numberErr, setNumberErr] = useState(false)
+  const validate = () => {
+    if (!validNumber.test(number)) {
+      setNumberErr(true)
+    }
+  };
   return (
     <div className="App">
      <div className="control has-icons-right">
+       <form>
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          value={number}
+          onChange={e => setNumber(e.target.value)}
 
         />
+         <button onClick={validate}>Submit</button>
+         {numberErr && <p>You must insert number</p>}
+       </form>
         <span className="icon is-small is-right">
           <i className="fas fa-times" />
         </span> 
---
+++ src/Regex.js
@@ -0,0 +1,1 @@
+export const validNumber = new RegExp('d+/d+');
