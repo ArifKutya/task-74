a0747bb24e3668dab1b992b60e51e5c545186b6b
Arif <arifkutq63@gmail.com>
2023-01-11 23:26:55 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import "./App.css";
 import { useState } from "react";
-import { validNumber } from "./Regex";
+
+
 
 function App() {
-  const [number, setNumber] =useState("")
-  const [numberErr, setNumberErr] = useState(false)
-  const validate = () => {
-    if (!validNumber.test(number)) {
-      setNumberErr(true)
-    }
-  };
+  const [number, setNumber] = useState("")
   return (
     <div className="App">
-     <div className="control has-icons-right">
-       <form>
-        <input
-          className="input is-large"
-          type="text"
-          placeholder="Enter number..."
-          value={number}
-          onChange={e => setNumber(e.target.value)}
-
-        />
-         <button onClick={validate}>Submit</button>
-         {numberErr && <p>You must insert number</p>}
-       </form>
+      <div className="control has-icons-right">
+        <form>
+          <input
+            className="input is-large"
+            type="text"
+            value={number}
+            onChange={(e) => {
+              setNumber(e.target.value)
+            }}
+            placeholder="Enter number..."
+          />
+          <p>NUmber: {number}</p>
+        </form>
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className="fas fa-times" id="MyElement" />
+        </span>
+        <button onClick={validateNumber}>Check</button>
       </div>
     </div>
+
   );
+  function validateNumber() {
+    if (!number.match(/^\d+/))
+    {
+      document.getElementById("MyElement").className = "fas fa-check";
+    }
+  }
+
 }
+
 
 export default App;
